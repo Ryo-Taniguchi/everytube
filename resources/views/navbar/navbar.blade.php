fc400ff9d68ef0b16131afc481294005c5983081
ryo.taniguchi <tng.am0607@gmail.com>
2019-01-25 07:04:42 +0000

    update navbar

--- resources/views/navbar/navbar.blade.php
+++ resources/views/navbar/navbar.blade.php
@@ -1,5 +1,5 @@
 <header class="mb-4">
-    <nav class="navbar navbar-expand-sm navbar-dark" style="background-color:#FF3333;">
+    <nav class="navbar navbar-expand-sm navbar-dark" style="background-color:#FF3333">
         <a class="navbar-brand" href="/">EveryTube</a>
         
         <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
@@ -9,8 +9,15 @@
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
-                <li>{!! link_to_route('signup.get','新規登録',[],['class' => 'nav-link']) !!}</li>
-                <li>{!!  link_to_route('login', 'ログイン', [], ['class'=> 'nav-link']) !!}</li>
+            @if(Auth::check())
+                <li class="nav-item"><a href="#" class="nav-link">ホーム</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">シェア</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">マイページ</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">ログアウト</a></li>
+            @else
+                <li class="nav-item">{!! link_to_route('signup.get','新規登録',[],['class' => 'nav-link']) !!}</li>
+                <li class="nav-item">{!! link_to_route('login', 'ログイン', [], ['class'=> 'nav-link']) !!}</li>
+            @endif
             </ul>
         </div>
     </nav>
