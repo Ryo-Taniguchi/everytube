c22e9ebb16ed6c64918a936760733bd49bfb5b2d
ryo.taniguchi <tng.am0607@gmail.com>
2019-02-17 17:23:05 +0000

    update favorites view

--- resources/views/users/favorites.blade.php
+++ resources/views/users/favorites.blade.php
@@ -3,30 +3,32 @@
 @section('content')
     <div class="profile">
         <div class="row">
-            <aside class="card">
-                <div class="card-header">
-                    <h3 class="card-title text-center">{{ $user->name }}</h3>
-                </div>
-                <div class="card-body">
-                    <div class="text-center">
-                        <img src="{{ $user->filename }}" width="250" height="200">
+            <aside class="col-sm-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title show-title text-center">{{ $user->name }}</h3>
                     </div>
-                    @if (Auth::id() == $user->id)
-                        {!! Form::open([ 'route' => ['user.upload',$user->id], 'method' => 'post', 'class' => 'form', 'files' => true]) !!}
-                            <div class="form-group">
-                                {!! Form::file('file', null) !!}
-                            </div>
-                            <div class="form-group">
-                                {!! Form::submit('アップロード',['class' => 'btn btn-outline-success']) !!}
-                            </div>
-                        {!! Form::close() !!}
-                    @endif
-                </div>
-                <div class="follow_button">
-                    @include('buttons.follow_button',['user' => $user])
+                    <div class="card-body">
+                        <div class="text-center">
+                            <img class="img-fluid" src="{{ $user->filename }}" width="220" height="250">
+                        </div>
+                        @if (Auth::id() == $user->id)
+                            {!! Form::open([ 'route' => ['user.upload',$user->id], 'method' => 'post', 'class' => 'form', 'files' => true]) !!}
+                                <div class="form-group">
+                                    {!! Form::file('file', null) !!}
+                                </div>
+                                <div class="form-group">
+                                    {!! Form::submit('アップロード',['class' => 'btn btn-outline-success']) !!}
+                                </div>
+                            {!! Form::close() !!}
+                        @endif
+                    </div>
+                    <div class="follow_button">
+                        @include('buttons.follow_button',['user' => $user])
+                    </div>
                 </div>
             </aside>
-            <div class="user-index">
+            <div class="user-index col-sm-8">
                 @include('users.followings',['followings' => $followings])
             </div>
         </div>
@@ -38,7 +40,7 @@
                     <li class="nav-item"><a href="{{ route('users.favorites',['id'=>$user->id])}}" class="nav-link {{ Request::is('users/*/favorites') ? 'active' : ''}}">お気に入り一覧 <span class="badge badge-secondary">{{ $count_favorites }}</span></a></li>
                 </ul>
                 <div class="video-content">
-                    <ul class="media-list videos">
+                    <ul class="videos">
                         @foreach ($videos as $video)
                             @if( $user->is_favorites($video->id) )
                                 @include('videos.videos', ['video' => $video])
